15a3c8f80f4952e00e34259c6bbcbb633c997c7b
Santii05-C <santi.c.pgm@gmail.com>
2024-10-01 16:43:10 -0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      {/* public routes */}
+      <Route path="/sing-in" element={<SingninForm />} />
+      {/* private routes */}
+      <Route index element={<Home />} />
+    </main>
+  );
 };
 
 export default App;
