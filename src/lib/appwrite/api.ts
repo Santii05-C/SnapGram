07a07572bda1a4da1e0717832ea7704f74f5d2e5
Santii05-C <santi.c.pgm@gmail.com>
2024-10-04 20:30:02 -0300

    last commit

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -54,10 +54,8 @@ export async function saveUserToDB(user: {
 
 export async function signInAccount(user: { email: string; password: string }) {
   try {
-    const session = await account.createEmailPasswordSession(
-      user.email,
-      user.password
-    );
+    const session = await account.createSession(user.email, user.password);
+
     return session;
   } catch (error) {
     console.log(error);
