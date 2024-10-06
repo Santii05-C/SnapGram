5e722933c14f4d0b72f027325ec14a181d66b063
Santii05-C <santi.c.pgm@gmail.com>
2024-10-06 18:46:50 -0300

    last commit

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,13 @@
-import { IContextType, IUser } from "@/types";
+import { getCurrentUser } from "@/lib/appwrite/api";
+import { IUser } from "@/types";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const INITIAL_USER = {
   id: "",
   name: "",
   username: "",
+  email: "",
   imageUrl: "",
   bio: "",
 };
@@ -18,6 +21,15 @@ const INITIAL_STATE = {
   checkAuthUser: async () => false as boolean,
 };
 
+type IContextType = {
+  user: IUser;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
+};
+
 const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
@@ -25,8 +37,26 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  const navigate = useNavigate();
+
   const checkAuthUser = async () => {
     try {
+      const currentAccount = await getCurrentUser();
+
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
+
+        setIsAuthenticated(true);
+        return true;
+      }
+      return false;
     } catch (error) {
       console.log(error);
       return false;
@@ -34,6 +64,16 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (
+      localStorage.getItem("cookiefallback") === "[]" ||
+      localStorage.getItem("cookieFallback") === null
+    )
+      navigate("/sign-in");
+
+    checkAuthUser();
+  }, []);
 
   const value = {
     user,
