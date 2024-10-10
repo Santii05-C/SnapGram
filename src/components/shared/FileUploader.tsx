a4d65f082461c64041438c965cbf36d07ec08d05
Santii05-C <santi.c.pgm@gmail.com>
2024-10-10 17:36:19 -0300

    last commit

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,5 +1,27 @@
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
 const FileUploader = () => {
-  return <div>FileUploader</div>;
+  const [fileUrl, setFileUrl] = useState("");
+
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? (
+        <div>test 1</div>
+      ) : (
+        <div className="file_uploader-box">test 2</div>
+      )}
+    </div>
+  );
 };
 
 export default FileUploader;
