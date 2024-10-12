491d8f4d82ff9e899db3f6b707d763273fa0c96a
Santii05-C <santi.c.pgm@gmail.com>
2024-10-12 18:26:56 -0300

    last commit

--- src/components/shared/PostStatus.tsx
+++ src/components/shared/PostStatus.tsx
@@ -1,5 +1,17 @@
-const PostStatus = () => {
-  return <div>PostStatus</div>;
+import { Models } from "appwrite";
+
+type PostStatsProps = {
+  post: Models.Document;
+};
+
+const PostStatus = ({ post, userId }: PostStatsProps) => {
+  return (
+    <div className="flex justify-between items-center z-20">
+      <div className="flex gap-2 mr-5">
+        <img src="/assets/icons/liked.svg" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default PostStatus;
