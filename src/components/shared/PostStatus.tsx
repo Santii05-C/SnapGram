6ba08d650f6ca44c0a00b37db0b6fb21dffcfeca
Santii05-C <santi.c.pgm@gmail.com>
2024-10-14 12:03:27 -0300

    last commit

--- src/components/shared/PostStatus.tsx
+++ src/components/shared/PostStatus.tsx
@@ -67,34 +67,34 @@ const PostStatus = ({ post, userId }: PostStatsProps) => {
   return (
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 mr-5">
-        {isSavingPost || isDeletingSaved ? (
-          <Loader />
-        ) : (
-          <img
-            src={
-              checkIsLiked(likes, userId)
-                ? "/assets/icons/liked.svg"
-                : "/assets/icons/like.svg"
-            }
-            alt="like"
-            width={20}
-            height={20}
-            onClick={handleLikePost}
-            className="cursor-pointer"
-          />
-        )}
+        <img
+          src={
+            checkIsLiked(likes, userId)
+              ? "/assets/icons/liked.svg"
+              : "/assets/icons/like.svg"
+          }
+          alt="like"
+          width={20}
+          height={20}
+          onClick={handleLikePost}
+          className="cursor-pointer"
+        />
         <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
 
       <div className="flex gap-2 ">
-        <img
-          src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
-          alt="like"
-          width={20}
-          height={20}
-          onClick={handleSavePost}
-          className="cursor-pointer"
-        />
+        {isSavingPost || isDeletingSaved ? (
+          <Loader />
+        ) : (
+          <img
+            src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
+            alt="like"
+            width={20}
+            height={20}
+            onClick={handleSavePost}
+            className="cursor-pointer"
+          />
+        )}
       </div>
     </div>
   );
