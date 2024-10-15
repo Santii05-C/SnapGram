8050158b6a9c16041feb89590813afc0e6611db9
Santii05-C <santi.c.pgm@gmail.com>
2024-10-15 16:36:30 -0300

    last commit

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -6,8 +6,8 @@ const Explore = () => {
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
-        <h2 className="h-3-bold md:h2-bold w-full">Search Posts</h2>
-        <div className="flex gap-1 px-4 w-full rounded-[5px] bg-dark-4">
+        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
+        <div className="flex gap-1 px-4 w-full rounded-[5px] bg-dark-4 ">
           <img
             src="/assets/icons/search.svg"
             width={24}
@@ -20,6 +20,19 @@ const Explore = () => {
             className="explore-search"
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h3 className="body-bold md:h3-bold">Popular Today </h3>
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4">
+          <p className="small-medium md:base-medium tesxt-light-2">All</p>
+          <img
+            src="/assets/icons/filter.svg"
+            width={20}
+            height={20}
+            alt="filter"
           />
         </div>
       </div>
