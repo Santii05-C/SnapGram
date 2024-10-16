5a13abf359dab65c0f8376d644782d0b63cfed76
Santii05-C <santi.c.pgm@gmail.com>
2024-10-16 12:02:20 -0300

    last commit

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,4 +1,13 @@
-const SearchResults = () => {
+import { Models } from "appwrite";
+
+type SearchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
+};
+const SearchResults = ({
+  isSearchFetching,
+  searchedPosts,
+}: SearchResultsProps) => {
   return <div>SearchResults</div>;
 };
 
